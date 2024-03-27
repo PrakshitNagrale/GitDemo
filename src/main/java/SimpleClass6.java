09a59f456d5726f94056a58d9f58029219abad82
Prakshit Nagrale <nagraleprakshit2@gmail.com>
2024-03-27 13:40:13 +0530

    changes in branch f3

--- src/main/java/SimpleClass6.java
+++ src/main/java/SimpleClass6.java
@@ -1,4 +1,41 @@
 public class SimpleClass6 {
 
-//this is for branh 3
+public static void main(String [] args){
+
+    //que--> Replace B with C only inside the brackets
+
+    String A="a[ab]ba[bab]cz";
+   // String A="e[exe]a";
+    String B="a";
+    String C="b";
+
+
+    char[] arr =A.toCharArray();
+    char b=B.charAt(0);
+    char c=C.charAt(0);
+
+
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]=='['){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]==b){
+                    arr[j]=c;
+                }
+                else if(arr[j]==']'){
+                    i=j;
+                    break;
+
+                }
+            }
+        }
+    }
+
+    System.out.println(String.valueOf(arr));
+
+
+    }
+
+
+
+
 }
